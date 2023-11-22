a86496815f8976182704f75938f2ea35ebdbf0b2
TommyRG <a_tomas.ruiz.gonzalez@iespablopicasso.es>
2023-11-22 09:37:36 +0100

    "Creamos el repositorio y añadimos la clase NumerosAleatorios que crea un listado de 20 números aleatorios entre 1 y 10."

---
+++ src/Act3GIT/NumerosAleatorios.java
@@ -0,0 +1,12 @@
+package Act3GIT;
+
+public class NumerosAleatorios {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 20;i++) {
+			System.out.println(Math.random() * 10);
+		}
+	}
+
+}
