66240b09b03daef1b2456eb1acf1ba4e51536e02
TommyRG <a_tomas.ruiz.gonzalez@iespablopicasso.es>
2023-11-22 09:38:13 +0100

    Añadimos clase NumerosAleatorios.

--- src/Act3GIT/NumerosAleatorios.java
+++ src/Act3GIT/NumerosAleatorios.java
@@ -4,7 +4,7 @@ public class NumerosAleatorios {
 
 	public static void main(String[] args) {
 		
-		for (int i = 0; i < 20;i++) {
+		for (int i = 0; i < 20; i++) {
 			System.out.println(Math.random() * 10);
 		}
 	}
